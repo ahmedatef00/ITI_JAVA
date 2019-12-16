f52bc8a18f5b7a16b0f774756d8e161dd161e49c
Ahmed Atef <ahmed62437@student.must.edu.eg>
2019-12-16 21:21:49 +0200

    first lab in java

---
+++ firstapp/src/firstapp/Firstapp.java
@@ -0,0 +1,14 @@
+package firstapp;
+
+public class Firstapp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//System.out.println(" Hello Java");
+for (int i = 0; i < args.length; i++)
+{
+    System.out.println("hello " + args[i]);
+}
+	}
+
+}
